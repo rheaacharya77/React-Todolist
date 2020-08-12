ffdddb6995390df08f6f4021b9f4ea948b1bfd84
rheaacharya77 <ria.acharya07@gmail.com>
2020-08-12 11:03:08 +0545

    Updates

---
+++ precache-manifest.02f0dc28ea92abeed11c08cc07f915a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e372de660c04672e1ae238b87ba77217",
+    "url": "/React-Todolist-Leapfrog/index.html"
+  },
+  {
+    "revision": "6e187e3355e7c6b98d1d",
+    "url": "/React-Todolist-Leapfrog/static/css/main.ae9aaec7.chunk.css"
+  },
+  {
+    "revision": "a5caa870cd2ad3910599",
+    "url": "/React-Todolist-Leapfrog/static/js/2.e5e2a8e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Todolist-Leapfrog/static/js/2.e5e2a8e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e187e3355e7c6b98d1d",
+    "url": "/React-Todolist-Leapfrog/static/js/main.d4769ec0.chunk.js"
+  },
+  {
+    "revision": "222b879e5130de276b3d",
+    "url": "/React-Todolist-Leapfrog/static/js/runtime-main.6ec19bae.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Todolist-Leapfrog/static/media/logo.5d5d9eef.svg"
+  }
+]);
